858ec25c0d1c82c878006858e35eff107b20ab91
bschwyn <ben.schwyn@gmail.com>
2018-04-10 15:32:38 -0700

    removed stuff

--- app/routes.py
+++ app/routes.py
@@ -27,20 +27,11 @@ def upload():
     </body>
 </html>"""
 
-#@app.route('/test/')
-#def test():
-#    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'picture.jpg')
-#    return render_template('test.html', bla = full_filename)
-
 @app.route('/test1/')
 def send_file():
     filename = 'picture.jpg'
     path = os.path.join(app.root_path, 'static')
     return send_from_directory(directory=path, filename=filename)
-
-#@app.route('<fname>')
-#def legacy_images(fname):
-#    return redirect(url_for('static', filename='static/' + fname), code=301)
 
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
@@ -50,11 +41,3 @@ def upload_file():
         f.save(os.path.join(target, f.filename))
     #do stuff with uploaded image
     return render_template('template.html', filename='picture.jpg')
-
-#figure out what request.files['file'] means
-#figure out where save file goes --- to project root!
-
-#how do I have the the app call a function
-#what is a simple thing that I can do with the image?
-#display the image.
-#have the image be available for download
