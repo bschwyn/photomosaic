40c2e1d3a9b118598206806244c54749ea63afc2
bschwyn <ben.schwyn@gmail.com>
2018-04-30 14:17:34 -0700

    upload internet images

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import Flask, request, render_template, send_from_directory, redirect, url_for
 from werkzeug import secure_filename
+import urllib.request
+import ntpath
 
 import os
 
@@ -22,36 +24,8 @@ def index():
 
 @app.route('/upload/')
 def upload():
-    return """
-<html>
-    <head>
-    <title>Photomosaic Image Upload </title>
-    <meta name ="description"
-    conent="TinEye is a reverse image search engine. Search by image: Give it an image and it will tell you where the image appears on the web."> 
-    </head>
-    <body>
-    
-    <form action = "http://localhost:5000/uploader" method = "POST"
-    enctype = "multipart/form-data">
-    <input type = "file" name = "file" />
-    <input type = "submit"/>
-    </form >
-    
-    <div class="forms ">
-        <form id="upload_form" method="post" action="http://localhost:5000/uploader" enctype="multipart/form-data">
-              <label for="upload_box" id="upload-button"></label>
-              <input id="upload_box" name="image" onchange="display_throbber(); this.form.submit()" title="Upload an image" type="file"  />
-              <input style="display: none;" id="file_submit" value="Upload Image" class="submit" type="submit"  />
-            </form>
-            <form id="url_form" method="post" action="https://www.tineye.com/search" onsubmit="display_throbber();">
-              <div class="input-container">
-                <input class="image-url" id="url_box" name="url" maxlength="100000" placeholder="Upload or enter Image URL"  onfocus="this.form.search_button.disabled=false" type="text"  />
-              </div>
-              <input class="submit-button" id="url_submit" name="search_button" value=""  type="submit" />
-            </form>
-        </div>
-    </body>
-</html>"""
+    return render_template("template4.html")
+
 
 @app.route('/test1/')
 def send_file():
@@ -68,6 +42,19 @@ def upload_file():
     #do stuff with uploaded image
     filename = f.filename
     return render_template('template.html', filename=filename)
+
+@app.route('/uploader_url', methods=['GET', 'POST'])
+def download_url_and_upload():
+    if request.method == 'POST':
+        url = request.form['url']
+
+        head, tail = ntpath.split(url)
+        local_filename = "/home/ben/Projects/photomosaic/app/static/" + tail
+        urllib.request.urlretrieve(url, local_filename)
+        return render_template('template.html', filename = tail)
+    if request.method == 'GET':
+        return 'get'
+    return "not post"
 
 @app.route('/rotate/<filename>', methods=['GET', 'POST'])
 def rotate(filename):
